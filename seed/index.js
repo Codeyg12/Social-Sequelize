93fe1527e93e915893619dc9b5b0a48d3df332ad
Codey Gallup <codey.gallup@gmail.com>
2024-11-07 10:21:14 -0700

    Adding associations and tests

---
+++ seed/index.js
@@ -0,0 +1,13 @@
+const userSeed = require("./users.json");
+const profileSeed = require("./profiles.json");
+const postSeed = require("./posts.json");
+const commentSeed = require("./comments.json");
+const likeSeed = require("./likes.json");
+
+module.exports = {
+  userSeed,
+  profileSeed,
+  postSeed,
+  commentSeed,
+  likeSeed,
+};
